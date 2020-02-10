5fc9f5a26b7a93637ee10cd986936908744f42f3
Davi Lobo <davi.lobo.gs@gmail.com>
2020-02-10 00:43:08 -0300

    Video modals now working

--- public/assets/js/video-modal.js
+++ public/assets/js/video-modal.js
@@ -1,15 +1,21 @@
-const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".card-item"); 
+
+const modalOverlay = document.querySelector(".modal-overlay");
+const modalVideo = modalOverlay.querySelector(".video");
 
 
 for (let card of cards) {
     card.addEventListener("click", function() {
+        const videoID = card.getAttribute("id");
         modalOverlay.classList.add("actived");
+        modalVideo.src = `https://www.youtube.com/embed/${videoID}`;
     });
-}
+};
+
 
 const closeButton = document.querySelector(".modal-overlay");
 
 closeButton.addEventListener("click", function() {
     modalOverlay.classList.remove("actived");
+    modalVideo.src = "";
 });
