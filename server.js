73cb9da42f40987dbc7d9c00404329e05bde7560
Davi Lobo <davi.lobo.gs@gmail.com>
2020-02-09 23:31:38 -0300

    Now made the server.js in express and nunjucks template engine added

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+
+const server = express();
+
+server.use(express.static('public'));
+
+server.set("view engine", "html");
+
+nunjucks.configure("views", {
+   express: server 
+});
+
+server.get("/", function(req, res) {
+    return res.render("about");
+});
+
+server.get("/videos", function(req, res) {
+    return res.render("videos");
+});
+
+server.listen(5000, function() {
+    console.log('Node server is running...')
+});
