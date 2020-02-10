a473c55a8fe4761962fbbfb9225ec4c5aefe9794
Davi Lobo <davi.lobo.gs@gmail.com>
2020-02-10 01:15:25 -0300

    Testing dinamic data

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "c7P03kkrEG8",
+        title: "Configurando VSCode",
+        duration: "22 min",
+        price: "Free"
+    },
+    {
+        id: "ICIz5dE3Xfg",
+        title: "Deploy de apps Node",
+        duration: "114 min",
+        price: "Free"
+    },
+    {
+        id: "X2RKRKdqqwM",
+        title: "Estrutura de pastas para React",
+        duration: "15 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid",
+        duration: "36 min",
+        price: "Free"
+    },
+    {
+        id: "aTf8QTjw4RE",
+        title: "Typescript com NodeJS",
+        duration: "43 min",
+        price: "Free"
+    },
+    {
+        id: "awRtgpRsdTQ",
+        title: "Recriando interface do Pipefy",
+        duration: "111 min",
+        price: "Free"
+    },
+];
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 
+const videos = require("./data");
 
 const server = express();
 
@@ -13,11 +14,22 @@ nunjucks.configure("views", {
 });
 
 server.get("/", function(req, res) {
-    return res.render("about");
+    const about = {
+        avatar_url: "https://avatars3.githubusercontent.com/u/42360847?s=460&v=4",
+        name: "Davi Lobo",
+        role: "Web Developer",
+        bio: "Web developer at Bleez E-commerce and Computer Science student at IFCE, Javascript, Node, React and R. Native in a near future.",
+        bio2: "In my free time, I also like to detonate on Guitar Hero.",
+        links: [
+            { name: "Github", url: "https://github.com/Davi-Lobo"},
+            { name: "Linkedin", url: "https://www.linkedin.com/in/davi-lobo-gomes/"},
+        ],
+    }
+    return res.render("about", { about });
 });
 
 server.get("/videos", function(req, res) {
-    return res.render("videos");
+    return res.render("videos", { items: videos });
 });
 
 server.listen(5000, function() {
